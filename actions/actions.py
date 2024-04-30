9c10d126eb29a55aaddc92d5d8de6591629f707c
unknown <arthur.heude@epsi.fr>
2024-05-01 00:42:08 +0200

    create other stories with basic response

--- actions/actions.py
+++ actions/actions.py
@@ -49,3 +49,38 @@ class CheckEnoughSpace(Action):
                 connection.close()
         SlotSet("story_number", False)
         return [SlotSet("story_number", False)]
+
+class CreateBooking(Action):
+    def name(self) -> Text:
+        return "action_create_booking"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        try:
+            slot_nb = tracker.get_slot("slot_nb")
+            slot_date = datetime.strptime(tracker.get_slot("slot_date"), "%d/%m/%Y").strftime('%Y-%m-%d')
+            slot_name = tracker.get_slot("slot_name")
+            slot_n_phone = tracker.get_slot("slot_n_phone")
+            
+            connection = psycopg2.connect(
+                dbname="boh8g92em20wuq1uhc3z",
+                user="uvrnmeghftjfmobhpj2k",
+                password="YdbB0WZR7g5kIbI32J53fKJ7JwcBLF",
+                host="boh8g92em20wuq1uhc3z-postgresql.services.clever-cloud.com",
+                port="50013"
+            )
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO booking.booking (c_name, nb_people, phone, date) VALUES (%s, %s, %s, %s) RETURNING ID;", (slot_name, slot_nb, slot_n_phone, slot_date))
+            connection.commit()
+            bookingId = cursor.fetchone()[0]
+            
+        except psycopg2.Error as e:
+            dispatcher.utter_message("Error connecting to the database: {}".format(e))
+            return []
+        finally:
+            if 'connection' in locals():
+                cursor.close()
+                connection.close()
+                
+        dispatcher.utter_message(f"Votre code de réservation est le suivant : {bookingId}")  
+        dispatcher.utter_message(f"Voulez-vous ajouter un commentaire à la réservation ?")
+        return []
